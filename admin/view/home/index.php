437a91e12e8e2a2d5073b7c5083ccc576291d873
nguyenduccong <71300677+nguyenduccong@users.noreply.github.com>
2020-11-18 08:12:16 +0700

    Create index.php

---
+++ admin/view/home/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+            <div class="full">
+                    <div class="top_ad_right">
+                    
+                    </div>
+                </div>
+
+                <div class="full">
+                    <div class="box_statis">
+                        <div class="box_statis_left">
+                            <div class="box_statis_top">
+                                <div class="box_statis1 box_sd"></div>
+                                <div class="box_statis2 box_sd"></div>
+                            </div>
+                            <div class="box_statis_bot box_sd">
+                                
+                            </div>
+                        </div>
+                        <div class="box_statis_right box_sd">
+                            
+                        </div>
+                    </div>
+                </div>
+</body>
+</html>
