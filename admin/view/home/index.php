b2ed7440d939afd0a28581494c63c27e2ca2075c
TramAnh1406 <71300653+TramAnh1406@users.noreply.github.com>
2020-11-18 08:14:53 +0700

    Update index.php

--- admin/view/home/index.php
+++ admin/view/home/index.php
@@ -28,5 +28,26 @@
                         </div>
                     </div>
                 </div>
+    //Phần của TA
+    <div class="boxright">
+           
+            <div class="place ml20">
+                <h1>PLACE</h1>
+                <div class="btn">
+                <a href="#" class="add_place">Add Place</a>
+                <a href="#" class="add_place">Edit Place</a>
+                <a href="#" class="add_place">Delete Place</a>
+                </div>
+                
+
+                <a href="index.php?ctrl=place" class="box_place1"></a>
+                <a href="#" class="box_place2"></a>
+                <a href="#" class="box_place3"></a>
+                <a href="#" class="box_place4"></a>
+
+                
+            </div>
+        </div>
+    //END TA
 </body>
 </html>
