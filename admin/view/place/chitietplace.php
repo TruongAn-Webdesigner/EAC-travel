83ea1a50b6e63b4fa70a348a5981013667dc5d3a
TramAnh1406 <71300653+TramAnh1406@users.noreply.github.com>
2020-11-18 09:00:38 +0700

    Update chitietplace.php

--- admin/view/place/chitietplace.php
+++ admin/view/place/chitietplace.php
@@ -1,1 +1,14 @@
-chit
+
+<div class="place ml20">
+                <h1>PLACE</h1>
+                
+
+                <div class="box_count_review"></div>
+                <div class="box_count_view_review"></div>
+                <div class="rank_review"></div>
+
+                <a href="#" class="box_review"></a>
+                <a href="#" class="box_review"></a>
+                <a href="#" class="box_review"></a>
+                <a href="#" class="box_review"></a>
+        </div>
